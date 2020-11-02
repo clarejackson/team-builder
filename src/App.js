db9372d6349a53940e46d0416131de6adcd1c2fa
Clare Jackson <clare-jackson@lambdastudents.com>
2020-11-01 21:34:49 -0500

    Got info to submit into state, but not rendered on the page.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import Form from './Form'
+import Form from './Form';
 
 const teamList = [
   {
@@ -20,29 +20,23 @@ const teamList = [
   }
 ]
 
-const initialFormValues = {
-  name: '',
-  email: '',
-  role: ''
-}
 
 function App() {
 
   const [teamMembers, setTeamMembers] = useState(teamList)
-  const [formValues, setFormValues] = useState(initialFormValues)
    
-  //useEffect to render team members
+ 
+  
   
   return (
     <div className="App">
       <h1>Team Builder Form</h1>
-      <Form />
+      <Form teamMembers={teamMembers} setTeamMembers={setTeamMembers} />
       {/* //map through results from useEffect to render li inside ul */}
       {teamList.map((teamMember, index) => {
-        return ([<h2 key={index}>{teamMember.name}</h2>,
-        <p key={index}>{teamMember.role}</p>,
-        <p key={index}>contact info: {teamMember.email}</p>
-        ])
+        return <><h2 key={index}>{teamMember.name}</h2>
+        <p key={index}>{teamMember.role}</p>
+        <p key={index}>contact info: {teamMember.email}</p></>
       })}
     </div>
   );
--- src/Form.js
+++ src/Form.js
@@ -1,37 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 const Form = (props) => {
 
+const [teammate, setTeammate] = useState({
+  name: '',
+  email: '',
+  role: ''
+})
+
+const { teamMembers, setTeamMembers } = props
+
   //handleChanges
+  const handleChanges = (event) => {
+    setTeammate({ ...teammate,[event.target.name]: event.target.value })
+  }
+  console.log(teamMembers)
+
+  // const addNewMember = (member) => {
+  //   const newMember = {
+  //     id: Date.now(),
+  //     name: member.name,
+  //     role: member.role,
+  //     email: member.email
+  //   }
+  // }
 
   //submitForm
+  const submitForm = (event) => {
+    event.preventDefault()
+    setTeammate({
+          id: Date.now(),
+          name: teamMembers.name,
+          role: teamMembers.role,
+          email: teamMembers.email
+        })
+    setTeamMembers([...teamMembers, teammate])
+  }
 
   return (
-    <form>
-      <label htmlFor="name">Name</label>
+    <form onSubmit={submitForm}>
+      <div>
+      <label htmlFor="name">Name     </label>
       <input 
       id="name"
       name="name"
       type="text"
       placeholder="Enter Name"
+      value={teamMembers.name}
+      onChange={handleChanges}
+      required
       />
-
-      <label htmlFor="email">Email</label>
-      <input 
-      id="email"
-      name="email"
-      type="email"
-      placeholder="Enter Email"
-      />
-
-<label htmlFor="role">Role</label>
+      </div>
+      <div>
+      <label htmlFor="role">Role     </label>
       <input 
       id="role"
       name="role"
       type="text"
       placeholder="Enter Roll"
+      value={teamMembers.role}
+      onChange={handleChanges}
+      required
       />
+      </div>
+      <div>
+      <label htmlFor="email">Email     </label>
+      <input 
+      id="email"
+      name="email"
+      type="email"
+      placeholder="Enter Email"
+      value={teamMembers.email}
+      onChange={handleChanges}
+      required
+      />
+      </div>
+
+      <button type="submit">Submit</button>
+
     </form>
   )
 }
